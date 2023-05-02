84d4841ff50dc5959c7dccbd4a92c043bdffa79a
Tulio Albuquerque <tulioalbuquerque@outlook.com>
2023-05-02 17:52:43 -0300

    feat: adds models, serializer and routes for list and create books

--- books/views.py
+++ books/views.py
@@ -1,3 +1,14 @@
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import generics
 from django.shortcuts import render
 
-# Create your views here.
+from .permissions import IsEmployeeOrReadOnly
+from .serializers import BookSerializer
+from .models import Book
+
+
+class BookView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsEmployeeOrReadOnly]
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
