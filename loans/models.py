644d8342fcaa7db315a9c0327767a77ff6b4a451
Giovanni Perotto <giovanniperotto@hotmail.com>
2023-05-03 10:25:23 -0300

    feat: create lean serializer and model

--- loans/models.py
+++ loans/models.py
@@ -3,4 +3,15 @@ from django.db import models
 
 # Create your models here.
 class Loan(models.Model):
-    ...
+    copy = models.ForeignKey(
+        "copies.Copy",
+        on_delete=models.CASCADE,
+        related_name="copy_loan",
+    )
+    loaner = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="user_copy_loan",
+    )
+
+    return_date = models.DateTimeField()
