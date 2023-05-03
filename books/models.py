a20d1584b3b7cdbf6cc108db8cb5a38f7c978c9b
Sarah Costa <sarahvenske@gmail.com>
2023-05-03 09:55:40 -0300

    feat: model following created

--- books/models.py
+++ books/models.py
@@ -6,3 +6,7 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     author = models.CharField(max_length=100)
+
+    users_following = models.ManyToManyField(
+        "users.User", related_name="books_following"
+    )
