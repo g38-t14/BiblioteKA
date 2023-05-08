84a1d0afe9b4cd8e8d3c57ed18e30c02642ff750
julianairana <julianairana@hotmail.com>
2023-05-08 12:24:50 -0300

    feat: loans list all and by id

--- loans/urls.py
+++ loans/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("loans/<int:loan_id>/return/", views.ReturnBookView.as_view()),
     path("users/loans/", views.LoanDetailView.as_view()),
+    path('loans/all/', views.LoanListView.as_view()),
+    path('users/<int:pk>/loans/', views.LoanListDetailView.as_view()),
 ]
