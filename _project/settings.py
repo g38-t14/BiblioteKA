7266487028104211ad039b0160b5d6ca29396191
Sarah Venske Costa <sarahvenske@gmail.com>
2023-05-08 10:23:21 -0300

    feat: token lifetime added

--- _project/settings.py
+++ _project/settings.py
@@ -124,6 +124,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+}
+
 REST_FRAMEWORK = {
     # "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     # "PAGE_SIZE": 2,
